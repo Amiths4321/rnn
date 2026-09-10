bd28539f7ee1d3178885e1827ed76369ac6bb153
amits <amiths@3beacons.com>
2026-09-10 15:43:11 +0530

    first commit

---
+++ rnn33.py
@@ -0,0 +1,17 @@
+from tensorflow import keras
+import numpy as np
+
+english_words = [
+    "i",
+    "drink",
+    "milk"
+]
+
+french_words = [
+    "je",
+    "bois",
+    "lait"
+]
+
+print("English:", english_words)
+print("French:", french_words)
