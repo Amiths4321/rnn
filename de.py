192abd78788469825b37357c1e44bb25c5160004
amit <amitchincholkar@gmail.com>
2026-09-11 10:12:49 +0530

    first commit

---
+++ de.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# Random latent representation
+latent = np.random.normal(
+    size=(5, 8)
+)
+
+print("Latent representations:")
+print(latent)
+
+print("\nShape:")
+print(latent.shape)
